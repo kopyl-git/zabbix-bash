34c616a5b508bff9bacd161a21bfb443f30ee73c
kopyl-git <gotgchi@gmail.com>
2024-06-23 20:55:30 +0300

    Update zabbix-add-host.sh

--- zabbix-add-host.sh
+++ zabbix-add-host.sh
@@ -23,13 +23,13 @@ remove_old_zabbix(){
 install_zabbix(){
     if [[ ! -f "/etc/zabbix/zabbix_agentd.conf" ]]; then
         apt remove --purge zabbix-agent -y
-        wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu20.04_all.deb
-        dpkg -i "zabbix-release_6.4-1+ubuntu20.04_all.deb"
+        wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu22.04_all.deb
+        dpkg -i "zabbix-release_6.4-1+ubuntu22.04_all.deb"
         apt-get update
         apt-get install -y zabbix-agent
         systemctl restart zabbix-agent
         systemctl enable zabbix-agent
-        rm -f "zabbix-release_6.4-1+ubuntu20.04_all.deb"
+        rm -f "zabbix-release_6.4-1+ubuntu22.04_all.deb"
     fi
 }
 
@@ -37,7 +37,7 @@ config_zabbix(){
     sed -i "s/Hostname=.*/Hostname=$hostname/g" /etc/zabbix/zabbix_agentd.conf
     sed -i "s/Server=.*/Server=$server_ip/g" /etc/zabbix/zabbix_agentd.conf
     sed -i "s/ServerActive=.*/ServerActive=$server_ip/g" /etc/zabbix/zabbix_agentd.conf
-    sed -i "s/# HostMetadata=/HostMetadata=alt.autoreg/" /etc/zabbix/zabbix_agentd.conf
+    sed -i "s/# HostMetadata=/HostMetadata=linux.autoreg/" /etc/zabbix/zabbix_agentd.conf
     systemctl restart zabbix-agent
 }
 
