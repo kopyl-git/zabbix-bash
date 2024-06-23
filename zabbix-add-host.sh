bf50e043d1da5b1254bce2e5d177d236ddc9a271
kopyl-git <gotgchi@gmail.com>
2024-06-23 20:54:15 +0300

    Update zabbix-add-host.sh

--- zabbix-add-host.sh
+++ zabbix-add-host.sh
@@ -9,7 +9,6 @@ zabbix_url="https://nodes.website/api_jsonrpc.php"
 zabbix_user="api_user"
 zabbix_pass="eZ57E5x55VAyZks"
 discovery_group_id="5"
-auth_token="0c7e1ec52e11bf40dc3b05abc2392c373711832111602ea4676b5a63475985ae"
 
 remove_old_zabbix(){
     if [[ $(systemctl list-units --full -all | grep "zabbix-agent" | wc -l) -ne 0 ]]; then
@@ -24,13 +23,13 @@ remove_old_zabbix(){
 install_zabbix(){
     if [[ ! -f "/etc/zabbix/zabbix_agentd.conf" ]]; then
         apt remove --purge zabbix-agent -y
-        wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu22.04_all.deb
-        dpkg -i "zabbix-release_6.4-1+ubuntu22.04_all.deb"
+        wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu20.04_all.deb
+        dpkg -i "zabbix-release_6.4-1+ubuntu20.04_all.deb"
         apt-get update
         apt-get install -y zabbix-agent
         systemctl restart zabbix-agent
         systemctl enable zabbix-agent
-        rm -f "zabbix-release_6.4-1+ubuntu22.04_all.deb"
+        rm -f "zabbix-release_6.4-1+ubuntu20.04_all.deb"
     fi
 }
 
@@ -42,53 +41,48 @@ config_zabbix(){
     systemctl restart zabbix-agent
 }
 
+# Отримання авторизаційного токена
+auth_token=$(curl -s -X POST -H 'Content-Type: application/json' -d '{
+    "jsonrpc": "2.0",
+    "method": "user.login",
+    "params": {
+        "username": "'$zabbix_user'",
+        "password": "'$zabbix_pass'"
+    },
+    "id": 1
+}' $zabbix_url | jq -r '.result')
+
 # Функція для отримання ID групи по її імені
 get_group_id() {
     local group_name=$1
-    local response=$(curl -s --request POST \
-        --url "$zabbix_url" \
-        --header "Authorization: Bearer ${auth_token}" \
-        --header 'Content-Type: application/json-rpc' \
-        --data '{
-            "jsonrpc": "2.0",
-            "method": "hostgroup.get",
-            "params": {
-                "filter": {
-                    "name": [
-                        "'$group_name'"
-                    ]
-                }
-            },
-            "id": 1
-        }')
-
-    # Діагностичне виведення для перевірки відповіді
-    echo "Get Group Response: $response"
-
-    local group_id=$(echo $response | jq -r '.result[0].groupid')
+    local group_id=$(curl -s -X POST -H 'Content-Type: application/json' -d '{
+        "jsonrpc": "2.0",
+        "method": "hostgroup.get",
+        "params": {
+            "filter": {
+                "name": [
+                    "'$group_name'"
+                ]
+            }
+        },
+        "auth": "'$auth_token'",
+        "id": 1
+    }' $zabbix_url | jq -r '.result[0].groupid')
     echo $group_id
 }
 
 # Функція для створення групи
 create_group() {
     local group_name=$1
-    local response=$(curl -s --request POST \
-        --url "$zabbix_url" \
-        --header "Authorization: Bearer ${auth_token}" \
-        --header 'Content-Type: application/json-rpc' \
-        --data '{
-            "jsonrpc": "2.0",
-            "method": "hostgroup.create",
-            "params": {
-                "name": "'$group_name'"
-            },
-            "id": 1
-        }')
-
-    # Діагностичне виведення для перевірки відповіді
-    echo "Create Group Response: $response"
-
-    local group_id=$(echo $response | jq -r '.result.groupids[0]')
+    local group_id=$(curl -s -X POST -H 'Content-Type: application/json' -d '{
+        "jsonrpc": "2.0",
+        "method": "hostgroup.create",
+        "params": {
+            "name": "'$group_name'"
+        },
+        "auth": "'$auth_token'",
+        "id": 1
+    }' $zabbix_url | jq -r '.result.groupids[0]')
     echo $group_id
 }
 
@@ -96,48 +90,33 @@ create_group() {
 move_host_to_group() {
     local hostname=$1
     local new_group_id=$2
-    local response=$(curl -s --request POST \
-        --url "$zabbix_url" \
-        --header "Authorization: Bearer ${auth_token}" \
-        --header 'Content-Type: application/json-rpc' \
-        --data '{
-            "jsonrpc": "2.0",
-            "method": "host.get",
-            "params": {
-                "filter": {
-                    "host": [
-                        "'$hostname'"
-                    ]
-                }
-            },
-            "id": 1
-        }')
-
-    # Діагностичне виведення для перевірки відповіді
-    echo "Get Host Response: $response"
-
-    local host_id=$(echo $response | jq -r '.result[0].hostid')
-
-    response=$(curl -s --request POST \
-        --url "$zabbix_url" \
-        --header "Authorization: Bearer ${auth_token}" \
-        --header 'Content-Type: application/json-rpc' \
-        --data '{
-            "jsonrpc": "2.0",
-            "method": "host.update",
-            "params": {
-                "hostid": "'$host_id'",
-                "groups": [
-                    {
-                        "groupid": "'$new_group_id'"
-                    }
+    local host_id=$(curl -s -X POST -H 'Content-Type: application/json' -d '{
+        "jsonrpc": "2.0",
+        "method": "host.get",
+        "params": {
+            "filter": {
+                "host": [
+                    "'$hostname'"
                 ]
-            },
-            "id": 1
-        }')
-
-    # Діагностичне виведення для перевірки відповіді
-    echo "Move Host Response: $response"
+            }
+        },
+        "auth": "'$auth_token'",
+        "id": 1
+    }' $zabbix_url | jq -r '.result[0].hostid')
+    curl -s -X POST -H 'Content-Type: application/json' -d '{
+        "jsonrpc": "2.0",
+        "method": "host.update",
+        "params": {
+            "hostid": "'$host_id'",
+            "groups": [
+                {
+                    "groupid": "'$new_group_id'"
+                }
+            ]
+        },
+        "auth": "'$auth_token'",
+        "id": 1
+    }' $zabbix_url > /dev/null
 }
 
 # Основний процес
@@ -158,3 +137,4 @@ if [ -z "$group_id" ]; then
 fi
 
 move_host_to_group "$hostname" "$group_id"
+
